1670130bd4b8c1eaecbf9c398c4c4468707240d8
unknown <285893578+yogidharmaa@users.noreply.github.com>
2026-06-12 14:09:28 +0700

    chore: clean up accidental files

--- tempCodeRunnerFile.go
+++
@@ -1,8 +0,0 @@
-		if tambah.namaPerangkat == dataPerangkat[i].namaPerangkat {
-			if tambah.ruangan == dataPerangkat[i].ruangan {
-				fmt.Print("Nama perangkat sudah ada diruangan yang sama")
-				pause()
-				return
-			}
-		}
-	}
